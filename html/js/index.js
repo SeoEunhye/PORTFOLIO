30794e4292c50308184b079cd9e878905110cac1
SeoEunhye <53415266+SeoEunhye@users.noreply.github.com>
2019-08-27 01:48:40 +0900

    Add files via upload

---
+++ html/js/index.js
@@ -0,0 +1,90 @@
+/*header Bar*/
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+/*header Bar*/
+
+/*pannel*/
+$(document).ready(function(){
+
+	//right 메뉴 펼쳤다가 끄기	
+	$(".rightMenuBg").hide();
+	$(".panel").click(function(){
+		$(".rightmenu").animate({
+			"width":"100%"
+		},500)
+		$(".rightMenuBg").fadeIn()
+	})
+	$(".closeBtn,.rightMenuBg").click(function(){
+		$(".rightmenu").animate({
+			"width":"0"
+		},500)
+		$(".rightMenuBg").fadeOut()
+	})
+})
+/*pannel*/
+/*Progress Bars*/
+		$(function(){
+    "use strict";
+    $(function() {
+        function animated_contents() {
+            $(".zt-skill-bar > div ").each(function (i) {
+                var $this  = $(this),
+                    skills = $this.data('width');
+
+                $this.css({'width' : skills + '%'});
+
+            });
+        }
+        
+        if(jQuery().appear) {
+            $('.zt-skill-bar').appear().on('appear', function() {
+                animated_contents();
+            });
+        } else {
+            animated_contents();
+        }
+    });
+}(jQuery));
+
+/*Radial Progress Bars*/
+$(function(){
+
+    // Remove svg.radial-progress .complete inline styling
+    $('svg.radial-progress').each(function( index, value ) { 
+        $(this).find($('circle.complete')).removeAttr( 'style' );
+    });
+
+    // Activate progress animation on scroll
+    $(window).scroll(function(){
+        $('svg.radial-progress').each(function( index, value ) { 
+            // If svg.radial-progress is approximately 25% vertically into the window when scrolling from the top or the bottom
+            if ( 
+                $(window).scrollTop() > $(this).offset().top - ($(window).height() * 0.75) &&
+                $(window).scrollTop() < $(this).offset().top + $(this).height() - ($(window).height() * 0.25)
+            ) {
+                // Get percentage of progress
+                percent = $(value).data('percentage');
+                // Get radius of the svg's circle.complete
+                radius = $(this).find($('circle.complete')).attr('r');
+                // Get circumference (2πr)
+                circumference = 2 * Math.PI * radius;
+                // Get stroke-dashoffset value based on the percentage of the circumference
+                strokeDashOffset = circumference - ((percent * circumference) / 100);
+                // Transition progress for 1.25 seconds
+                $(this).find($('circle.complete')).animate({'stroke-dashoffset': strokeDashOffset}, 1250);
+            }
+        });
+    }).trigger('scroll');
+
+});
+/*Radial Progress Bars*/
+
+/*pofolBg*/
+
+
